3171fdc2814f12595e5edf05dd7d06f7ea4a920b
Cereno Deng <cereno.deng@outlook.com>
2023-09-23 17:49:35 +0800

    create server to response from file

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Prize Draw Nimble");
+const server = Bun.serve({
+  hostname: 'localhost',
+  port: '3300',
+  fetch: fetchHandler
+});
+console.log(`Prize Draw Nimble is running on ${server.hostname}:${server.port}`);
+
+async function fetchHandler(request) {
+  const url = new URL(request.url);
+  if (url.pathname == "/") {
+    return new Response(Bun.file('index.html'));
+  }
+}
