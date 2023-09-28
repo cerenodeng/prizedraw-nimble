c6e8b91d96aa554340aa683ad9d54a0448282631
Cereno Deng <cereno.deng@outlook.com>
2023-09-29 00:32:54 +0800

    create 404 response

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const server = Bun.serve({
-  hostname: 'localhost',
-  port: '3300',
-  fetch: fetchHandler
+	hostname: 'localhost',
+	port: '3300',
+	fetch: fetchHandler,
 });
 console.log(`Prize Draw Nimble is running on ${server.hostname}:${server.port}`);
 
 async function fetchHandler(request) {
-  const url = new URL(request.url);
-  if (url.pathname == "/") {
-    return new Response(Bun.file('index.html'));
-  }
+	const url = new URL(request.url);
+	if (url.pathname === '/') {
+		return new Response(Bun.file('index.html'));
+	}
+	return new Response(Bun.file('404.html'), { status: 404 });
 }
