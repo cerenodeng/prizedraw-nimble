1d6f9509edc3ca7d1673cc7482659f5c7250591a
Cereno Deng <cereno.deng@outlook.com>
2023-09-30 17:23:45 +0800

    create asset response

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ async function fetchHandler(request) {
 	const url = new URL(request.url);
 	if (url.pathname === '/') {
 		return new Response(Bun.file('index.html'));
+	} else if (url.pathname.startsWith('/assets/') && request.method === 'GET') {
+		return new Response(Bun.file('.' + url.pathname));
+	} else if (url.pathname === '/users') {
 	}
 	return new Response(Bun.file('404.html'), { status: 404 });
 }
